cb717045f06047b4caef5c291ce52fa59abc4d5b
LeopoldoBini <87399582+LeopoldoBini@users.noreply.github.com>
2021-08-10 04:19:22 -0300

    quedo header

--- js/script.js
+++ js/script.js
@@ -1,37 +1,35 @@
-window.onscroll = function(){
-
+window.onscroll = function (){
     scroll = document.documentElement.scrollTop;
 
     header = document.getElementById("header");
 
-    if (scroll > 20){
-        header.classList.add('nav_mod');
-    }else if(scroll < 20){
-        header.classList.remove('nav_mod');
+    if(scroll > 20){
+        header.classList.add("apretar__nav")
+    }else if(scroll < 20){ 
+        header.classList.remove("apretar__nav")
     }
-
 }
 
 document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
 
 menu = document.getElementById("header");
-body = document.getElementById("container_all");
-nav = document.getElementById("nav");
+nav = document.getElementById( "nav");
+body = document.getElementById( "contenedorDeTodo");
 
-function mostrar_menu(){
+function mostrar_menu (){
+
+    menu.classList.toggle("mover__contenido");
+    body.classList.toggle("mover__contenido");
+    nav.classList.toggle("mover__nav");
     
-    menu.classList.toggle('move_content');
-    body.classList.toggle('move_content');
-    nav.classList.toggle('move_nav');
 }
-
 
 window.addEventListener("resize", function(){
 
     if(window.innerWidth > 760){
-        menu.classList.remove('move_content');
-    body.classList.remove('move_content');
-    nav.classList.remove('move_nav');
-    }
+        menu.classList.remove("mover__contenido");
+        body.classList.remove("mover__contenido");
+        nav.classList.remove("mover__nav");
 
+    }
 })
